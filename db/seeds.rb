42566616d3f6abbf4e6fb6f537ed3989a6e23fc8
Meagan Butters <meagan.butters@hotmail.com>
2020-12-18 14:29:42 -0500

    removed unneccessary data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,6 @@
 
 me = User.create(email: 'meagan@email.com', password: 123456)
 
-# waffle = Cone.create(name: "Waffle Cone")
-# normal = Cone.create(name: "Normal Cone")
-# bowl = Cone.create(name: "Bowl")
-
-# soft = IceCreamType.create(name: "Soft")
-# hard = IceCreamType.create(name: "Hard")
-
-# chocolate = IceCreamFlavour.create(name: "Chocolate")
-# vanilla = IceCreamFlavour.create(name: "Vanilla")
-# brownie = IceCreamFlavour.create(name: "Brownie")
-# cookie_dough = IceCreamFlavour.create(name: "Cookie Dough")
-# mint = IceCreamFlavour.create(name: "Mint")
-# oreo = IceCreamFlavour.create(name: "Oreo")
-
-# marshmallows = Topping.create(name: "Marshmallows")
-# cereal = Topping.create(name: "Cereal")
-# skore = Topping.create(name: "Skore Bits")
-# oreo = Topping.create(name: "Oreo")
-
-# sundae = Sundae.create(name: "The meagan", )
-
 require 'csv'
 
 csv_options = { col_sep: ',', headers: :first_row }
